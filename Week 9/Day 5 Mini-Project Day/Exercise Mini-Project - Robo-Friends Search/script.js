0b3a5d71c683aec2f6d80830132a4f688cd9904e
Yuveshi <gyuveshi987@gmail.com>
2024-02-07 11:03:38 +0100

    Line starting

---
+++ Week 9/Day 5 Mini-Project Day/Exercise Mini-Project - Robo-Friends Search/script.js
@@ -0,0 +1,110 @@
+// What You Will Create
+// A Robo-Friends Search with Advanced Javascript
+
+// In this project you will have to create a robot website (you can use your own css to style your project):
+
+const robots = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      image: 'https://robohash.org/1?200x200'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv',
+      image: 'https://robohash.org/2?200x200'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net',
+      image: 'https://robohash.org/3?200x200'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org',
+      image: 'https://robohash.org/4?200x200'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca',
+      image: 'https://robohash.org/5?200x200'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info',
+      image: 'https://robohash.org/6?200x200'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz',
+      image: 'https://robohash.org/7?200x200'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me',
+      image: 'https://robohash.org/8?200x200'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io',
+      image:'https://robohash.org/9?200x200'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz',
+      image:'https://robohash.org/10?200x200'
+    }
+    ];
+function displayRobots (robots){
+    for (const robot of robots){
+        const html = 
+        `<div ><img id="cardImage" src="${robot.image}"  alt=""></div>
+        <h2>${robot.name}</h2>
+        <p>${robot.email}</p>`;
+
+        const newDiv = document.createElement('div');
+        newDiv.id = 'card';
+        newDiv.innerHTML = html;
+
+        document.querySelector('#content').appendChild(newDiv) 
+    }
+}
+
+displayRobots(robots)
+    const input = document.querySelector('input')
+
+    input.addEventListener('keyup', handleEnter)
+
+    function handleEnter(e) {
+        e.preventDefault;
+        const value = e.target.value;
+        const filterRobot = robots.filter((robot) => robot.name.toLowerCase().includes(value.toLowerCase()));
+        console.log(filterRobot);
+        eraseContent();
+        displayRobots(filterRobot);
+    }
+
+    function eraseContent(){
+        document.querySelector('#content').innerHTML = ''
+    }
+   
