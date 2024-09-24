e16663fbff841c2371576e2b7d40e5f844ed5eb8
Yuveshi <gyuveshi987@gmail.com>
2024-09-25 01:36:05 +0200

    w12d4votingapp

---
+++ WEEK 12 REACT Framework/Day 4 React Components And State/Daily Challenge Voting App/voting-app/src/components/Language.component.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Language.component.css";
+
+export class Language extends React.Component {
+  constructor(props) {
+    console.log("props:", props);
+    super(props);
+  }
+
+handleClick = () => {
+this.props.incrementCount();
+};
+
+  render() {
+    return (
+      <>
+        <div className="language">
+          <span>{this.props.votes}</span>
+          <span>{this.props.name}</span>
+          <button onClick={this.handleClick}>Click Here</button>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Language;
